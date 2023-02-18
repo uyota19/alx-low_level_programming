19a5a54be2e04e5a8b4349c9ac6e670c10af5dfb
uyota19 <uyota19@gmail.com>
2023-02-17 17:04:33 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prrogram to print a string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
