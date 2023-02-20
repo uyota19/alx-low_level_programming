bcd01bf94809d0780af0805953a8ccff0f59f106
uyota19 <uyota19@gmail.com>
2023-02-20 14:45:57 -0800

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program to print all possible combos of single decimals'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		if (i < 9)
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
