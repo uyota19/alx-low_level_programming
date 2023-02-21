df1eb115cd3983106c8f223bf6d1e92b4c7bd66e
uyota19 <uyota19@gmail.com>
2023-02-20 19:59:52 -0800

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program to print all combos of three digits'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 7; i++)
+	{
+		for (int j = i + 1; j <= 8; j++)
+		{
+			for (int k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
