bc1a86f98a577b30dacb6b016075bd42f4e4564a
uyota19 <uyota19@gmail.com>
2023-02-22 15:45:33 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of a number
+ * @n: value to be used
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
