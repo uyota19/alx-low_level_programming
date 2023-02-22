0109c1d545d03f8b37d0c88e33f33c405c76380f
uyota19 <uyota19@gmail.com>
2023-02-22 02:12:48 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program that prints string '_putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	printf("%s\n", str);
+	return (0);
+}
