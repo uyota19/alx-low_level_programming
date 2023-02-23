de88465e05c268d569cf7659210a8c84a4a8d7a7
uyota19 <uyota19@gmail.com>
2023-02-23 02:47:01 -0800

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9 followed by a newline.
+ * Return: void.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n + '0');
+	_putchar('\n');
+}
