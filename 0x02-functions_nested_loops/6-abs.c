63d3ee211f3a6e1d695853a757b85ae5ef964f92
uyota19 <uyota19@gmail.com>
2023-02-22 16:29:43 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Print the absolute values of numbers
+ * @i: value to be used.
+ * Return: Always 0.
+ */
+
+int _abs(int i)
+{
+	return (i < 0 ? -i : i);
+	return (0);
+}
