007cf0980968d74a96c93d049aab22d9fdb42872
uyota19 <uyota19@gmail.com>
2023-02-23 04:18:36 -0800

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number from 0 to 14; with a newline
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
