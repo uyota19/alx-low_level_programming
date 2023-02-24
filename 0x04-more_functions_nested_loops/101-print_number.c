a7cda4d40604554e4e83b038020c94f9f0673497
uyota19 <uyota19@gmail.com>
2023-02-24 15:15:00 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,20 +8,12 @@
 
 void print_number(int n)
 {
-
-	int divisor = 1;
-
 	if (n < 0)
 	{
 		_putchar(45);
 		n = -n;
 	}
-	while (n / divisor >= 10)
-		divisor *= 10;
-	while (divisor > 0)
-	{
-		_putchar('0' + n / divisor);
-		n %= divisor;
-		divisor /= 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
