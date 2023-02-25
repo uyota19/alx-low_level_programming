8420a4da58d9b60b622c5bceeafb6336e4cef94e
uyota19 <uyota19@gmail.com>
2023-02-24 17:12:20 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
 		if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 		if (n % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", n);
+			printf("%d", n);
 		if (n != 100)
 			printf(" ");
 	}
