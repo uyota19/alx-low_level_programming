6c176928f438835078fca050962525f11e44e57d
uyota19 <uyota19@gmail.com>
2023-02-27 08:17:42 -0800

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of n to 98.
+ *
+ * @n: pointer value to be updated.
+ *
+ * Return: nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
