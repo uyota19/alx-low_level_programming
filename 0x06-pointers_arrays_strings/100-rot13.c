0cd28d9968b85989685b0aa83ac9239ae314a74e
uyota19 <uyota19@gmail.com>
2023-03-03 15:16:36 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,22 +10,24 @@ char *rot13(char *str)
 {
 	int i;
 	int j;
-	char *result;
-	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	result = str;
+	char *result = str;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; input[j] != '\0'; j++)
+		for (j = 0; j < 13 && str[i] != '\0'; j++)
 		{
-			if (str[i] == input[j])
-			{
-				str[i] = output[j];
-				break;
-			}
+			if ((str[i] >= 'a' && str[i] <= 'm')
+			|| (str[i] >= 'A' && str[i] <= 'M'))
+				result[i] = str[i] + 13;
+			else
+			if ((str[i] >= 'n' && str[i] <= 'z')
+			|| (str[i] >= 'N' && str[i] <= 'Z'))
+				result[i] = str[i] - 13;
+			else
+				result[i] = str[i];
+			i++;
 		}
+		i--;
 	}
 	return (result);
 }
