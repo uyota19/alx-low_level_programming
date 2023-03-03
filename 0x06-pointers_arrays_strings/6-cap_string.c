8dd039786fa3bd9d2e20a2bd6f766120f7b2474e
uyota19 <uyota19@gmail.com>
2023-03-03 14:41:50 -0800

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * cap_strtring - capitalizestr all wordstr in a string
+ * cap_string - capitalizestr all wordstr in a string
  * @str: the string to capitalize
  * Return: the capitalized string
  */
 
 char *cap_string(char *str)
 {
-	int i;
-	char strep[] = " \t\n,;.!?\"(){}";
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str && str[i] != '\0')
 	{
-		/* Capitalize firstrt letter of each word */
-		if ((i == 0 || str[i - 1] == strep[0] || str[i - 1] == strep[1] || str[i - 1] == strep[2] || str[i - 1] == strep[3] || str[i - 1] == strep[4] || str[i - 1] == strep[5] || str[i - 1] == strep[6] || str[i - 1] == strep[7] || str[i - 1] == strep[8] || str[i - 1] == strep[9] || str[i - 1] == strep[10] || str[i - 1] == strep[11]) && (str[i] >= 'a' && str[i] <= 'z'))
+		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t'
+			|| str[i - 1] == '\n' || str[i - 1] == ','
+			|| str[i - 1] == ';' || str[i - 1] == '.'
+			|| str[i - 1] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '"' || str[i - 1] == '('
+			|| str[i - 1] == ')' || str[i - 1] == '{'
+			|| str[i - 1] == '}') && (str[i] >= 'a'
+			&& str[i] <= 'z'))
 		{
-			str[i] -= 32;
+			str[i] = str[i] - 32;
 		}
-		/* Convert all other letterstr to lowercastre */
-		else
-		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			str[i] += 32;
-		}
+		i++;
 	}
 	return (str);
 }
