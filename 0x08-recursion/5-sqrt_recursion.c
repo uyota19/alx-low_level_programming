5c661f53d7a6beae0d7332d845e16d07fa3b51ab
uyota19 <uyota19@gmail.com>
2023-03-08 14:22:28 -0800

    101-wildcmp

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,26 +8,16 @@
 
 int _sqrt_recursion(int n)
 {
-	int left = 1;
-	int right = n;
+	int sqrtPrev = _sqrt_recursion(n - 1);
 
-	if (n == 0 || n == 1)
-		return (n);
 	if (n < 0)
 		return (-1);
-	while (left <= right)
+	if (n == 0 || n == 1)
+		return (n);
+	if (sqrtPrev * sqrtPrev == n - 1)
 	{
-		int mid = (left + right) / 2;
-
-		if (mid * mid == n)
-		{
-			return (mid);
-		}
-		else
-		if (mid * mid < n)
-			left = mid + 1;
-		else
-			right = mid - 1;
+		return (sqrtPrev + 1);
 	}
-	return (-1);
-}
+	else
+		return (sqrtPrev);
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that returns 1 if the input integer is
+ * a prime number, otherwise return 0
+ * @n: input integer
+ * Return: 1
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	
+
