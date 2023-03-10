fd984e64b78b61d73363d6f75f637f7135ef9424
uyota19 <uyota19@gmail.com>
2023-03-10 14:11:37 -0800

    README.md file added

--- 0x08-recursion/100-is_palindrome.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/**
- * is_palindrome - function that returns 1 if a string is a palindrome an
- * 0 if not
- * @s: a string
- * Return: 1 if palindrome is found and 0 if otherwise
- */
-
