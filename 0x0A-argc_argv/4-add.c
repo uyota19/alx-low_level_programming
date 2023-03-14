84b06a3ce451e9565def00553720d0c943228c31
uyota19 <uyota19@gmail.com>
2023-03-14 03:03:12 -0700

    Malloc Readme.md file added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - program that adds positive numbers
@@ -11,8 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-	int add, i;
+	int sum, i, j, num;
 
-	if (argc <=1)
-		printf("%d", 0);
-
+	if (argc == 1)
+	{
+		putchar(48);
+		putchar('\n');
+	}
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			num = 0;
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				num = num * 10 + (argv[i][j] - '0');
+			}
+			sum += num;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
